ce0b11f355a92e3c8bf3e05629c572aec7003058
Kyle <kylespinaio@hotmail.com>
2021-02-10 20:11:50 -0500

    components build in a statit manner

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@
 import './App.css';
 import React from "react";
 import Header from "./components/Header"
+import Figure from "./components/Figure"
+import WrongLetters from "./components/WrongLetters"
+import Word from "./components/Word"
+import PopUp from "./components/PopUp"
+
+
+
 
 function App() {
   return (
     <>
      <Header/>
+     <div className="game-container">
+       <Figure/> 
+       <WrongLetters/>
+       <Word/>
+     </div>
     </>
   );
 }
