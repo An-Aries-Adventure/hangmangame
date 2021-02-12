928cfe8481151bfa663536f577c421ecd878a929
Kyle <kylespinaio@hotmail.com>
2021-02-11 19:55:12 -0500

    need to rework wrong letter component. not displaying wrong letter and right letter.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header"
 import Figure from "./components/Figure"
 import WrongLetters from "./components/WrongLetters"
@@ -16,8 +16,34 @@ let selectedWord = words[Math.floor(Math.random() * words.length)];
 function App() {
 
 const [playable, setPlayable] = useState(true);
-const [correctLetters, setCorrectLetters] = useState([])
-const [wrongLetters, setWrongLetters] = useState([])
+const [correctLetters, setCorrectLetters] = useState([]);
+const [wrongLetters, setWrongLetters] = useState([]);
+
+window.addEventListener('keydown', e => {
+	if (playable) {
+		if (e.keyCode >= 65 && e.keyCode <= 90) {
+			const letter = e.key.toLowerCase();
+
+			if (selectedWord.includes(letter)) {
+				if (!correctLetters.includes(letter)) {
+					correctLetters.push(letter);
+
+					// displayWord();
+				} else {
+					// showNotification();
+				}
+			} else {
+				if (!wrongLetters.includes(letter)) {
+					wrongLetters.push(letter);
+
+					// updateWrongLettersEl();
+				} else {
+					// showNotification();
+				}
+			}
+		}
+	}
+});
 
   return (
     <>
